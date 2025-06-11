0f47c2440c388d9f96aadfd329e4eb6bf1741ef2
DearKurumi <nichesfh@gmail.com>
2025-06-11 21:00:20 +0800

    111

--- backend/app/upload.py
+++ backend/app/upload.py
@@ -257,8 +257,8 @@ def import_project_data(file):
 
                 # 提取 ID
                 leader_ids = extract_ids(row.get('leader', '').split('、'))
-                member_ids = extract_ids(row.get('members', '').split('、'))
-                teacher_ids = extract_ids(row.get('teachers', '').split('、'))
+                member_ids = extract_ids(row.get('member', '').split('、'))
+                teacher_ids = extract_ids(row.get('teacher', '').split('、'))
 
                 # 检查是否所有人存在于 Student/Teacher 表中
                 all_valid = True
